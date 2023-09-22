01990d10de37b6187b70037fef53f39d68fe15af
RogueStar112 <hildtgunnt@gmail.com>
2023-09-22 10:56:53 +0100

    Solved Problem 3

--- js/app.js
+++ js/app.js
@@ -47,22 +47,51 @@ testMultiply(5, 9);
 
 /////////////////////////////////////
 /* Problem 3
-Write a function called sumAndMultiply() that takes in three numbers as separate arguments and returns an array where the first element is the sum of those three numbers, the second element is the product of those three numbers,  and the third and fourth elements are strings that EXACTLY follow this example and use the values that were input into the function:
+Write a function called sumAndMultiply() that takes in three numbers as separate arguments and 
+returns an array where the first element is the sum of those three numbers, 
+the second element is the product of those three numbers,  
+and the third and fourth elements are strings that EXACTLY follow this example 
+and use the values that were input into the function:
 
 Third element: "4 and 7 and 5 sum to 16."
 Fourth element: "The product of 4 and 7 and 5 is 140."
 
-IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this function. To do addition, use your sum() function, and to do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this. However, you may continue to use the + operator for string concatenation.
+IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this function. 
+To do addition, use your sum() function, and to do multiplication, use your multiply() function that you've already created. 
+You're going to have to be resourceful to figure out how to do this. 
+However, you may continue to use the + operator for string concatenation.
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
 
 // Write your code here
 function sumAndMultiply(a, b, c) {
-  //eslint-disable-line
+  let num1_sum = sum(a, b);
+  let num2_sum = sum(0, c);
+
+  let num1_product = multiply(a, b);
+  let num2_product = multiply(1, c);
+
+  let result_sum = sum(num1_sum[0], num2_sum[0]);
+  let result_product = multiply(num1_product[0], num2_product[0]);
+
+  // console.log("RESULT SUM:", result_sum);
+  // console.log("RESULT PRODUCT: ", result_product);
+
+  let message_for_sum = `${a} and ${b} and ${c} sum to ${result_sum[0]}.`;
+  let message_for_product = `The product of ${a} and ${b} and ${c} is ${result_product[0]}.`;
+
+  // console.log("MFS", message_for_sum);
+  // console.log("MFP", message_for_product);
+  return [
+    result_sum[0],
+    result_product[0],
+    message_for_sum,
+    message_for_product,
+  ];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4, 7, 5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
