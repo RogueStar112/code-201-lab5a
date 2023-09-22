d5dcd4accd9d91c8fb4fa5846547faf1c3825e2e
RogueStar112 <hildtgunnt@gmail.com>
2023-09-22 11:47:24 +0100

    Solved Problem 5

--- js/app.js
+++ js/app.js
@@ -137,7 +137,9 @@ testSumArray(testArray);
 
 /////////////////////////////////////
 /* Problem 5
-Write a function called multiplyArray() that takes an array of numbers as its argument and returns an array whose first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
+Write a function called multiplyArray() that takes an array of numbers as its argument 
+and returns an array whose first element is the product of those numbers,
+ and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
 
 "The numbers 2,3,4 have a product of 24."
 
@@ -147,11 +149,21 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) {
-  //eslint-disable-line
+  let multiplyArrayString = multArr.toString();
+
+  // total is 1 so that multiplication can start. You cant multiply starting 0.
+  let total = 1;
+  for (let i = 0; i < multArr.length; i++) {
+    total *= multiply(1, testArray[i])[0];
+  }
+
+  let message = `The numbers ${multiplyArrayString} have a product of ${total}.`;
+
+  return [total, message];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop.
 
